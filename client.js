fefb664d6670379546396f89e9d583c3c56280ea
Sharmarke Duale <sduale01@gmail.com>
2018-12-15 19:34:14 -0600

    created a class to store employee info in new array

--- client.js
+++ client.js
@@ -1,15 +1,35 @@
-$(document).ready(function() {
-    $('#submitButton').on('click', addEmployeeToList);
+class Employee {
+    constructor (firstName, lastName, id, title, annualSalary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.title = title;
+        this.annualSalary = annualSalary;
+    }
+}
 
-
-    $('#employeeList').on('click', '.deleteEmployeeBtn', deleteTheEmployee);
-    
-})
+let employees = [];
 
 let monthyCost = 0
 
 
+$(document).ready(function() {
+
+    // submit button
+    $('#submitButton').on('click', addEmployeeToList);
+
+    // delete button
+    $('#employeeList').on('click', '.deleteEmployeeBtn', deleteTheEmployee);
+    
+})
+
+
+
+
 function addEmployeeToList() {
+    // Get employee info and add to class
+    let newEmployee = new Employee($('.firstNameInput').val(), $('.lastNameInput').val(), $('.idInput').val(), $('.titleInput').val(), $('.annualSalaryInput').val())
+    employees.push(newEmployee);
     //Adds employee to list
     $('#employeeList').append(`<li>
      ${$('.firstNameInput').val()}
@@ -20,15 +40,16 @@ function addEmployeeToList() {
      <button class="deleteEmployeeBtn">delete</button>
     </li>`);
 
+    // calculates monthly cost of each employee and adds it to monthyCost
     let monthlySalary = ($('.annualSalaryInput').val()) / 12
     monthyCost += monthlySalary;
 
     // changes the total monthly cost when a new employee's salary is added
-    $('#monthlyCost').html(`Total Monthly: $ ${monthyCost} `)
+    $('#monthlyCost').html(`Total Monthly: $ ${(monthyCost.toFixed(2))} `)
     
-    // changes backgroung of total monthly to red if greater than 20,000
+    // changes color of total monthly to red if greater than 20,000
     if (monthyCost > 20000) {
-        $('#monthlyCost').css('background-color', 'red');
+        $('#monthlyCost').css('color', 'red');
     }
 
     // clear input fields
